bb2a0d3ad6a724e1f9e66d81c3f5637901859aef
Jose Carvalho <jose.carvalho-ext@man.eu>
2019-08-15 11:36:48 +0100

    style polishing

--- src/UserInput/UserInput.js
+++ src/UserInput/UserInput.js
@@ -2,9 +2,20 @@ import React from "react";
 import "./UserInput.css"
 
 const userInput = props => {
+
+    const style = {
+        border: "1px solid #222aaa",
+        boxShadow: "0 3px 7px #0c0f3b"
+    }
+
     return (
         <div className="UserInput">
-            <p>username: <input onChange={props.onChange} value={props.username}></input></p>
+            <p>username:   <input type="text" 
+                       style={style}
+                       onChange={props.onChange} 
+                       value={props.username}>
+                </input>
+            </p>
         </div>
     )
 };
